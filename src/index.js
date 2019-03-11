2ca092197bfac6de8f8080989b7221d7da257571
Luiz Fellype <luiz.cassago@xdevel.com.br>
2019-03-11 10:31:34 -0300

    feat: add css prime from the sigma template

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import 'primereact/resources/themes/nova-light/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import './layout/layout.css'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
